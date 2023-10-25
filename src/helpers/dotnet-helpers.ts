fb0b6afcd0e73834639106bb3b1105384efd42c1
Brian Welsh <brian-kc@outlook.com>
2023-10-25 08:34:13 -0500

    update multi-output

--- src/helpers/dotnet-helpers.ts
+++ src/helpers/dotnet-helpers.ts
@@ -2,6 +2,7 @@ import * as core from '@actions/core'
 import { ToolRunner } from '@actions/exec/lib/toolrunner'
 
 import * as safeWhich from '@chrisgavin/safe-which'
+import { stderr } from 'process'
 
 export async function getDotnet(): Promise<string> {
   return await safeWhich.safeWhich('dotnet')
@@ -9,20 +10,26 @@ export async function getDotnet(): Promise<string> {
 
 export async function getDotnetVersion(): Promise<string> {
   let version: string = ''
-  await new ToolRunner(
-   await getDotnet(),
-   ['-v'],
-   {
-    silent: true,
-    listeners: {
-      stdout: (data) => {
-        version = data.toString()
-      },
-      stderr: (data) => {
-        version = data.toString()
+  let stderr: string = ''
+
+  try {
+    await new ToolRunner(
+    await getDotnet(),
+    ['-v'],
+    {
+      silent: true,
+      listeners: {
+        stdout: (data) => {
+          version += data.toString()
+        },
+        stderr: (data) => {
+          stderr += data.toString()
+        }
       }
     }
-   }
-  ).exec()
-  return version
+    ).exec()
+    return version.trim()
+  } catch (c) {
+    return stderr
+  }
 }
