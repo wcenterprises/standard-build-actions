0849ff05cb6414fda5e27860ab88e666031ac789
Brian Welsh <brian-kc@outlook.com>
2023-11-02 17:48:43 -0500

    feature(setup): create setup action

---
+++ dist/main.js
@@ -0,0 +1,52 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.run = void 0;
+const core = __importStar(require("@actions/core"));
+const wait_1 = require("./wait");
+/**
+ * The main function for the action.
+ * @returns {Promise<void>} Resolves when the action is complete.
+ */
+async function run() {
+    try {
+        const ms = core.getInput('milliseconds');
+        // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
+        core.debug(`Waiting ${ms} milliseconds ...`);
+        // Log the current timestamp, wait, then log the new timestamp
+        core.debug(new Date().toTimeString());
+        await (0, wait_1.wait)(parseInt(ms, 10));
+        core.debug(new Date().toTimeString());
+        // Set outputs for other workflow steps to use
+        core.setOutput('time', new Date().toTimeString());
+    }
+    catch (error) {
+        // Fail the workflow run if an error occurs
+        if (error instanceof Error)
+            core.setFailed(error.message);
+    }
+}
+exports.run = run;
+//# sourceMappingURL=main.js.map
