195ea7110d37c63ceb18ccb2b551b97fa2896b41
Brian Welsh <brian-kc@outlook.com>
2023-11-04 16:40:27 -0500

    update tests

---
+++ src/dotnet-action.ts
@@ -0,0 +1,51 @@
+import * as core from '@actions/core'
+import { IEnvironment } from './interfaces/environment'
+import { loadEnvironment } from './helpers/cache-utils'
+import { safeWhich } from '@chrisgavin/safe-which'
+/**
+ * The main function for the action.
+ * @returns {Promise<void>} Resolves when the action is complete.
+ */
+export async function run(command: string): Promise<void> {
+  try {
+    core.debug('Entering dotnet-action')
+    const env: IEnvironment = loadEnvironment(process.env['sba.environment'] as string)
+    core.debug('environment loaded...')
+
+    const dotnet = safeWhich('dotnet')
+    console.debug(`dotnet: ${dotnet}`)
+
+    switch (command) {
+      case 'restore': {
+        return runRestoreCommand()
+      }
+      case 'build': {
+        return runBuildCommand()
+      }
+      case 'pack': {
+        return runPackCommand()
+      }
+      default: {
+        throw new Error(`dotnet command '${command}' not implemented!`)
+      }
+    }    
+  } catch (error) {
+    // Fail the workflow run if an error occurs
+    /* istanbul ignore next */
+    if (error instanceof Error) core.setFailed(error.message)
+  }
+}
+
+export async function runRestoreCommand(): Promise<void> {
+
+}
+
+export async function runBuildCommand(): Promise<void> {
+
+}
+
+export async function runPackCommand(): Promise<void> {
+
+}
+
+run(core.getInput('command'))
