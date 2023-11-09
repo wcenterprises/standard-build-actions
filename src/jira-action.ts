2ce616f8dfae22ccf4ff4a4c42447562566bf022
Brian Welsh <brian-kc@outlook.com>
2023-11-09 08:40:19 -0600

    initial cor jira ticketing

---
+++ src/jira-action.ts
@@ -0,0 +1,30 @@
+import * as core from '@actions/core'
+
+/**
+ * The main function for the action.
+ * @returns {Promise<void>} Resolves when the action is complete.
+ */
+export async function run(command: string): Promise<void> {
+  try {
+    core.debug('Entering jira-action')
+
+    switch (command) {
+      case 'create': {
+        return
+      }
+      case 'get': {
+        return
+      }
+      case 'update': {
+        return
+      }
+    }
+
+  } catch (error) {
+    // Fail the workflow run if an error occurs
+    /* istanbul ignore next */
+    if (error instanceof Error) core.setFailed(error.message)
+  }
+}
+
+run(core.getInput('command'))
