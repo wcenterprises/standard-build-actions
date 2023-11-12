8ab26108a08fcb15a6aa1561d8e2bfe3eff39d20
Brian Welsh <brian-kc@outlook.com>
2023-11-12 14:53:16 -0600

    daily:update for issue creation

--- src/classes/jclient.ts
+++ src/classes/jclient.ts
@@ -26,14 +26,14 @@ export class JiraClient {
   async createIssue(key: string, summary: string, options?: CreateIssueOptions): Promise<string | undefined > {
 
     const adf = options?.markdown ? null : ch.translateMarkdown(String(options?.markdown))
+    console.log(`adf: ${adf}`)
 
     const result: jira.IssueObject = await this._api.addNewIssue(
       {
         fields: {
           issueType: { name: "Task" },
           project: { key: key },
-          summary: summary,
-          description: adf
+          summary: summary
         }
     })
     return result?.key as string
